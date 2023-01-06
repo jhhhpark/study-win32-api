747fd44c988d8e63823a0d6ff33ba9901c7af8d7
jhhhpark <znzldh1234@gmail.com>
2023-01-06 11:11:59 +0900

    [chapter 5] 리소스

---
+++ Chapter05/Accelerator/Accelerator.cpp
@@ -0,0 +1,75 @@
+#include <Windows.h>
+#include "resource.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("Accelerator");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS wndClass;
+	g_hInst = hInstance;
+
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.lpszClassName = lpszClass;
+	wndClass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	RegisterClass(&wndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	HACCEL hAccel;
+	hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+
+	while (GetMessage(&Message, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(hWnd, hAccel, &Message))
+		{
+			TranslateMessage(&Message);
+			DispatchMessage(&Message);
+		}
+
+	}
+
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMessage)
+	{
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_FILE_MENU1:
+			MessageBox(hWnd, TEXT("첫 번째 메뉴를 선택했습니다."), TEXT("Menu Demo"), MB_OK);
+			break;
+		case ID_FILE_MENU2:
+			MessageBox(hWnd, TEXT("두 번째 메뉴를 선택했습니다."), TEXT("Menu Demo"), MB_OK);
+			break;
+		case ID_FILE_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
